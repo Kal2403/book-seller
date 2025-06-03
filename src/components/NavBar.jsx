187095b719e3ddfacba0597ca091d00d6d0dcb70
Cristhian Castillo <kal2403199255@gmail.com>
2025-06-03 23:29:18 +0200

    Add logo section with Link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
+import { navbarStyles } from '../assets/dummystyles';
 import { Link, useLocation } from 'react-router-dom';
+import logo from '../assets/logoicon.png';
 
 const NavBar = () => {
 
@@ -15,7 +17,22 @@ const NavBar = () => {
 
     return (
         <nav className={navbarStyles.nav(scrolled)}>
-
+            <div className={navbarStyles.container}>
+                <div className='flex items-center justify-between'>
+                    <Link to='/' className={navbarStyles.logoContainer}>
+                        <div className='relative group'>
+                            <div className={navbarStyles.logoGradient} />
+                            <div className='relative flex items-center'>
+                                <img src={logo} alt='Logo' className={navbarStyles.logoImage} />
+                                <div className='ml-2'>
+                                    <h1 className={navbarStyles.logoText}>BOOKSHELL</h1>
+                                    <div className={navbarStyles.logoUnderline} />
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+                </div>
+            </div>
         </nav>
     )
 }
