133e3ac5ba6ca333241a6066f3c3e22a85c057c0
Cristhian Castillo <kal2403199255@gmail.com>
2025-06-08 22:44:46 +0200

    Implement loadInitialState from localStorage

--- src/CartContext/CartContext.jsx
+++ src/CartContext/CartContext.jsx
@@ -2,6 +2,21 @@ import { createContext, useContext } from "react";
 
 const CartContext = createContext();
 
+const loadInitalState = () => {
+    if (typeof window !== "undefined") {
+        const saved = localStorage.getItem("cart");
+
+        try {
+            const parsed = saved ? JSON.parse(saved) : null;
+            if (parsed && Array.isArray(parsed.items)) return parsed;
+            return { items: [] }
+        } catch {
+            return { items: [] }
+        }
+    }
+    return { items: [] }
+}
+
 const cartReducer = (state, action) => {
     switch (action.type) {
         default:
