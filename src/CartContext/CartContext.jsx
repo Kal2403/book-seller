885ab83968615daaf599684e2e6da8b26a24c425
Cristhian Castillo <kal2403199255@gmail.com>
2025-06-08 22:42:39 +0200

    Add cartReducer skeleton

--- src/CartContext/CartContext.jsx
+++ src/CartContext/CartContext.jsx
@@ -2,6 +2,13 @@ import { createContext, useContext } from "react";
 
 const CartContext = createContext();
 
+const cartReducer = (state, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
 export const CartProvider = ({ children }) => {
     return (
         <CartContext.Provider value={{}}>
