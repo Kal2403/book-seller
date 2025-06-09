84aab139350e44be1cf83ce6ebe91a76352f9f7a
Cristhian Castillo <kal2403199255@gmail.com>
2025-06-09 23:51:10 +0200

    Add heading and description text to Banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -19,6 +19,25 @@ const Banner = () => {
                     <div className='absolute -top-10 -right-10 md:-top-20 md:-right-20 w-48 h-48 md:w-96 md:h-96 bg-[#F8FFAE]/10 rounded-full blur-xl md:blur-3xl' />
                     <div className='absolute -bottom-20 -left-20 md:-bottom-40 md:-left-40 h-40 w-40 md:w-80 md:h-80 bg-[#43C6AC]/10 rounded-full blur-xl md:blur-3xl' />
                 </div>
+                <div className='grid lg:grid-cols-2 gap-8 md:gap-12 items-center'>
+                    <div className='space-y-6 md:space-y-8'>
+                        <div className='space-y-4 md:space-y-6'>
+                            <h1 className={headerText}>
+                                <span className='text-transparent bg-clip-text bg-gradient-to-r from-[#285876] to-[#43C6AC]'>
+                                    Mindful
+                                </span>
+                                <br />
+                                <span className={subHeader}>
+                                    Reading Experience
+                                </span>
+                            </h1>
+                            <p className={paragraphText}>
+                                Curated knowledge journey that challenge perceptons and inspire growth.
+                                Discover transformative content crefted for the modern intellect.
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
