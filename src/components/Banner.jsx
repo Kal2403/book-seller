1d4918ac87967eaaec58802342817310bc679ebd
Cristhian Castillo <kal2403199255@gmail.com>
2025-06-09 23:48:22 +0200

    Create basic Banner component structure with imported styles and assets

---
+++ src/components/Banner.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+import { 
+    container, formContainer, geometricOverlay, glassBox, headerText, imageSection, 
+    imageStyle, imageWrapper, inputField, inputWrapper, overlayEffect, paragraphText, 
+    scrollText, scrollTextSection, searchButton, statBox, statLabel, statNumber, 
+    statsContainer, subHeader 
+} from '../assets/dummystyles';
+import { useNavigate } from 'react-router-dom';
+import { Search } from 'lucide-react';
+import { words } from '../assets/dummydata';
+import img from '../assets/banner1.png';
+
+const Banner = () => {
+    
+    return (
+        <div className={container}>
+        </div>
+    )
+}
+
+export default Banner;
