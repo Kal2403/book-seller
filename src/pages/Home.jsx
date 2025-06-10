e817daad8fce8e10c70dee92b6b3a311fc028e85
Cristhian Castillo <kal2403199255@gmail.com>
2025-06-10 23:39:56 +0200

    Add OurBestSellers to Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from '../components/NavBar';
 import Banner from '../components/Banner';
+import OurBestSellers from '../components/OurBestSellers';
 
 const Home = () => {
     return (
         <>
             <NavBar />
             <Banner />
+            <OurBestSellers />
         </>
     )
 }
