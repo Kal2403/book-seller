e67b14e660047ebcb290a125ede6c2dbaefd3482
Cristhian Castillo <kal2403199255@gmail.com>
2025-06-10 23:29:27 +0200

    Create initial structure for OurBestSellers component

---
+++ src/components/OurBestSellers.jsx
@@ -0,0 +1,15 @@
+import React, { useRef } from 'react';
+import { ourBestSellersStyles as styles } from '../assets/dummystyles';
+import { ChevronLeft, ChevronRight, Minus, Plus, ShoppingCart, Star } from 'lucide-react';
+import { bgColors, obsbooks } from '../assets/dummydata';
+import { useCart } from '../CartContext/CartContext';
+
+const OurBestSellers = () => {
+
+    return (
+        <section className={styles.section}>
+        </section>
+    )
+}
+
+export default OurBestSellers;
