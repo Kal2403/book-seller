2188b77baebe3f663af33592799966df1a822291
Cristhian Castillo <kal2403199255@gmail.com>
2025-06-11 23:52:21 +0200

    Create initial structure for HomeBook component

---
+++ src/components/HomeBook.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { homeBooksStyles as styles } from '../assets/dummystyles';
+import { useCart } from '../CartContext/CartContext';
+import { hbbooks } from '../assets/dummydata';
+import { ArrowRight, Minus, Plus, ShoppingCart, Star } from 'lucide-react';
+import { Link } from 'react-router-dom';
+
+const HomeBook = () => {
+
+    return (
+        <div className={styles.section}>
+        </div>
+    )
+}
+
+export default HomeBook;
