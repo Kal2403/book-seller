158e732298ab43caca01cd828841cbcc0a8af79d
Cristhian Castillo <kal2403199255@gmail.com>
2025-06-11 23:53:26 +0200

    Add header section to HomeBook component

--- src/components/HomeBook.jsx
+++ src/components/HomeBook.jsx
@@ -9,6 +9,14 @@ const HomeBook = () => {
 
     return (
         <div className={styles.section}>
+            <div className={styles.container}>
+                <div className={styles.card}>
+                    <div className='text-center mb-12'>
+                        <h2 className={styles.heading}>Bookseller Favorites</h2>
+                        <div className={styles.headingLine} />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
