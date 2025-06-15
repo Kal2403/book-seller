de7c32783df81f5d05a17b142bc9d8f944c1d7c8
Cristhian Castillo <kal2403199255@gmail.com>
2025-06-15 23:34:00 +0200

    Create initial structure for HomeAbout component

---
+++ src/components/HomeAbout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { homeAboutStyles as s } from '../assets/dummystyles';
+import HomeAboutImage from '../assets/HomeAboutImage.png';
+import { featuredBooks, hastats } from '../assets/dummydata';
+import { Link } from 'react-router-dom';
+import { ArrowRight } from 'lucide-react';
+
+const HomeAbout = () => {
+    return (
+        <div className={s.wrapper}>
+        </div>
+    )
+}
+
+export default HomeAbout;
