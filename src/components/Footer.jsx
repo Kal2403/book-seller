b75bde1a17133a6e75627a4fa52d88a0f29032b7
Cristhian Castillo <kal2403199255@gmail.com>
2025-06-16 23:25:11 +0200

    Create initial structure for Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { footerStyles as styles } from '../assets/dummystyles';
+import { Link } from 'react-router-dom';
+import { quickLinks, socialLinks } from '../assets/dummydata';
+import { ArrowRight, Mail, MapPin, Phone } from 'lucide-react';
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+        </footer>
+    )
+}
+
+export default Footer;
