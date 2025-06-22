7c4b7e495e25c65237cc55edfd96f7e42dc95cce
Cristhian Castillo <kal2403199255@gmail.com>
2025-06-22 23:26:45 +0200

    Add initial layout for SignUp component

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import { Signup } from '../assets/dummystyles';
+import { Link, useNavigate } from 'react-router-dom';
+import { ArrowLeft, Eye, EyeOff, Lock, Mail, User } from 'lucide-react';
+
+const SignUp = () => {
+
+    return (
+        <div className={Signup.container}>
+
+        </div>
+    )
+}
+
+export default SignUp;
