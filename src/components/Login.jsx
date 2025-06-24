00e401d94260fc8b3d75bcc59d0e44e4bb94bef0
Cristhian Castillo <kal2403199255@gmail.com>
2025-06-24 23:30:17 +0200

    Add Login component skeleton

---
+++ src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from 'react';
+import { loginStyles } from '../assets/dummystyles';
+import { Link, useNavigate } from 'react-router-dom';
+import { ArrowRight, Eye, EyeOff, Lock, Mail } from 'lucide-react';
+
+const Login = () => {
+
+    return (
+        <div className={loginStyles.container}>
+
+        </div>
+    )
+}
+
+export default Login;
