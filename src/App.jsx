2bd5f0dd83e18f4c09bfa08d7f529f1fc86998ae
Cristhian Castillo <kal2403199255@gmail.com>
2025-06-24 23:37:20 +0200

    Add Login to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './components/Login';
 import SignUp from './components/SignUp';
 
 const App = () => {
     return (
         <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
             <Route path='/signup' element={<SignUp />} />
         </Routes>
     )
