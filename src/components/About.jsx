f59079b445c58f70b05987fa15fbdd3731046d38
Cristhian Castillo <kal2403199255@gmail.com>
2025-06-30 23:26:30 +0200

    Add hero section with heading and subtext

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,21 @@ import { Clock, Facebook, Instagram, MapPin, Twitch } from 'lucide-react';
 const About = () => {
     return (
         <div className={aboutStyles.container}>
-
+            <section className={aboutStyles.section}>
+                <div className={aboutStyles.innerContainer}>
+                    <div className={aboutStyles.headingWrapper}>
+                        <div className='relative inline-block'>
+                            <h1 className={aboutStyles.heading}>
+                                Crafting Literary <br /> Futures
+                            </h1>
+                            <div className={aboutStyles.underline} />
+                        </div>
+                        <p className={aboutStyles.subText}>
+                            Pioneering the next chapter in global storytelling. We bridge imagination with innovation through curated literary experiences.
+                        </p>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
