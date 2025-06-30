bc1c7e4536417b6a3711a258d7826c507821aeac
Cristhian Castillo <kal2403199255@gmail.com>
2025-06-30 23:24:56 +0200

    Add About component skeleton

---
+++ src/components/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import aboutStyles from '../assets/dummystyles';
+import { apbranches, apstats, apteamMembers } from '../assets/dummydata';
+import aboutUsImage from '../assets/AboutUsImage.png'
+import { Clock, Facebook, Instagram, MapPin, Twitch } from 'lucide-react';
+
+const About = () => {
+    return (
+        <div className={aboutStyles.container}>
+
+        </div>
+    )
+}
+
+export default About;
