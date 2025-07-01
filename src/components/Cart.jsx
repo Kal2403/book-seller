b84be8c6c35f6685ccc29cd33a89a877cb64c6a5
Cristhian Castillo <kal2403199255@gmail.com>
2025-07-01 23:08:54 +0200

    Add Cart component skeleton

---
+++ src/components/Cart.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+import { styles } from '../assets/dummystyles';
+import { ArrowRight, BookOpen, Link, Minus, Plus, ShoppingBag, Trash } from 'lucide-react';
+import { useCart } from '../CartContext/CartContext';
+
+const Cart = () => {
+
+    return (
+        <div className={styles.container}>
+
+        </div>
+    )
+}
+
+export default Cart;
