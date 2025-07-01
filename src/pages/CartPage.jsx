a2b1e23ea8510fdc698a03683b0ea08f162b61fb
Cristhian Castillo <kal2403199255@gmail.com>
2025-07-01 23:16:26 +0200

    Create CartPage and Add layout components NavBar, Cart, and Footer

---
+++ src/pages/CartPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import Footer from '../components/Footer';
+import Cart from '../components/Cart';
+
+const CartPage = () => {
+    return (
+        <>
+            <NavBar />
+            <Cart />
+            <Footer />
+        </>
+    )
+}
+
+export default CartPage;
