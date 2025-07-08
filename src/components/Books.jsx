a4c0f0a066a8db312cfdbad1a5ce26dceb8fb62b
Cristhian Castillo <kal2403199255@gmail.com>
2025-07-08 23:34:56 +0200

    Initial Component & Core Imports

---
+++ src/components/Books.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { ShoppingBag, Plus, Minus, Star, Search } from 'lucide-react';
+import { useLocation } from 'react-router-dom';
+import { booksPageStyles as styles } from '../assets/dummystyles';
+import { useCart } from '../CartContext/CartContext';
+
+const Books = () => {
+
+    return (
+        <div className={styles.container}>
+        </div>
+    )
+}
+
+export default Books;
