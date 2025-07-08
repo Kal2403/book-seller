0a44d5993038ae2333b4a295f5f18df91403822f
Cristhian Castillo <kal2403199255@gmail.com>
2025-07-08 23:35:32 +0200

    Add All Book Image Imports

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -2,6 +2,22 @@ import React, { useState } from 'react';
 import { ShoppingBag, Plus, Minus, Star, Search } from 'lucide-react';
 import { useLocation } from 'react-router-dom';
 import { booksPageStyles as styles } from '../assets/dummystyles';
+import BP1 from '../assets/BP1.png';
+import BP2 from '../assets/BP2.png';
+import BP3 from '../assets/BP3.png';
+import BP4 from '../assets/BP4.png';
+import BP5 from '../assets/BP5.png';
+import BP6 from '../assets/BP6.png';
+import BP7 from '../assets/BP7.png';
+import BP8 from '../assets/BP8.png';
+import BP9 from '../assets/BP9.png';
+import BP10 from '../assets/BP10.png';
+import BP11 from '../assets/BP11.png';
+import BP12 from '../assets/BP12.png';
+import BP13 from '../assets/BP13.png';
+import BP14 from '../assets/BP14.png';
+import BP15 from '../assets/BP15.png';
+import BP16 from '../assets/BP16.png';
 import { useCart } from '../CartContext/CartContext';
 
 const Books = () => {
